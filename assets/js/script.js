5e851d773a93df5ca6ad1931634935587c1a3a0e
Zofia <z.przybyl11@gmail.com>
2022-05-23 11:03:12 +0000

    Add first question and first image

---
+++ assets/js/script.js
@@ -0,0 +1,39 @@
+
+[
+    {
+      "question-area": "Where are those craters?"
+      "image": <https://cdn.pixabay.com/photo/2016/11/21/17/46/craters-1846775_1280.jpg>
+      "options": [
+        "Japan", "Hawaii", "Indonesia"
+      ]
+      "answer": "Hawaii"
+    },
+    
+    {
+      "question-area": "Where is this beach?"
+      "image": <https://cdn.pixabay.com/photo/2015/10/09/21/08/whitsundays-australia-979934_1280.jpg>
+      "options": [
+        "Australia", "Turkey", "Brazil"
+      ]
+      "answer" : "Australia"
+    },
+
+    {
+      "question-area": "Where is this?"
+      "image": <https://cdn.pixabay.com/photo/2018/07/09/06/23/vietnam-3525452_1280.jpg>
+      "options": [
+        "China", "Vietnam", "Philippines"
+      ]
+      "answer" : "Vietnam"
+    },
+
+    {
+      "question-area": "Where are those mountains?"
+      "image": <https://cdn.pixabay.com/photo/2020/02/14/07/15/mountains-4847687_1280.jpg>
+      "options": [
+        "Arizona", "China", "Peru"
+      ]
+      "answer" : "China"
+    },
+    
+  ]
