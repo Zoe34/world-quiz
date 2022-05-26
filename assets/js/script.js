9ab45c9b6b64da1d08363ddc1f8f39752c559815
Zofia <z.przybyl11@gmail.com>
2022-05-26 23:06:33 +0000

    Resize image

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,41 @@
 
 
+    
+
+    function overlayOff(){
+        let overlay = document.getElementById("overlay");
+        let start = document.getElementsByClassName("start");
+
+        overlay.style.display = "none";
+        start.addEventListener("click", overlayOn());
+        
+    }
+
+    function overlayOn(){
+        document.getElementById("overlay").style.display = "block";
+    }
+
+
+    [
+        {
+          "question": "Where are those craters?"
+          "image": <url>
+          "options": [
+            "Hawaii", "Japan", "Indonesia"
+          ]
+          "answer" : "Hawaii"
+        },
+        
+        {
+          "question": "Where is this beach?"
+          "image": <url>
+          "options": [
+            "Australia", "Turkey", "Brazil"
+          ]
+          "answer" : "Australia"
+        },
+        
+      ]
 
     
 
