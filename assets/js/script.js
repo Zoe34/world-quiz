0b658a835891454cbcf6ded95537de999b7a88e3
Zofia <z.przybyl11@gmail.com>
2022-05-27 16:18:40 +0000

    Create function to show the correct confirmation overlay for the correct answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,4 @@
 
-
-    
-
     function overlayOff(){
         let overlay = document.getElementById("overlay");
         let start = document.getElementsByClassName("start");
@@ -9,11 +6,34 @@
         overlay.style.display = "none";
         start.addEventListener("click", overlayOn);
         
+        
     }
 
     function overlayOn(){
         document.getElementById("overlay").classList.remove("hide");
+        document.getElementById("mainButtons").style.display = "none";        
     }
+
+    function correctOff(){
+        let correctPage = getElementById("correctPage");
+        let hawaii = getElementById("hawaii");
+
+        correctPage.style.display = "hide";
+        hawaii.addEventListener("click", correctOn);
+    }
+
+    function correctOn(){
+
+        document.getElementById("correctPage").classList.remove("hide");
+
+        let overlay = document.getElementById("overlay");
+        overlay.style.display = "none";
+
+    }
+
+
+
+
 
 
     
