5d53ddee9b0b3a7b662f246a82817c3306753053
Zofia <z.przybyl11@gmail.com>
2022-06-01 11:26:15 +0000

    Add fourth question to html and add a hide class to it

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,26 @@
 
+function helpOff(){
+    let helpPage = document.getElementById("helpPage");
+    helpPage.style.display = "none";
+    let help = document.getElementsByClassName("help");
+    help.addEventListener("click", helpOn);
+}
+
+function helpOn(){
+    document.getElementById("helpPage").classList.remove("hide");
+    document.getElementById("mainButtons").style.display = "none";
+}
+
+function backOff(){
+    let backButton = document.getElementsByClassName("backButton");
+    backButton.addEventListener("click", backOn);
+}
+
+function backOn(){
+  
+}
+
+
 function overlayOff() {
     let overlay = document.getElementById("overlay");
     let start = document.getElementsByClassName("start");
@@ -12,12 +34,32 @@ function overlayOn() {
     document.getElementById("mainButtons").style.display = "none";
 }
 
+function overlayTwoOff(){
+    let overlayTwo = document.getElementById("overlayTwo");
+    overlayTwo.style.display = "none";
+    let next = document.getElementsByClassName("next");
+    next.addEventListener("click", overlayTwoOn)
+    }
+    
+function overlayTwoOn(){
+    document.getElementById("overlayTwo").classList.remove("hide");
+    document.getElementById("incorrectPage").style.display = "none";
+    document.getElementById("correctPage").style.display = "none";
+}
+
+
+
+
+
 function correctOff() {
     let correctPage = document.getElementById("correctPage");
     correctPage.style.display = "none";
     
     let hawaii = document.getElementById("hawaii");
     hawaii.addEventListener("click", correctOn);
+
+    let australia = document.getElementById("australia");
+    australia.addEventListener("click", correctOn);
 }
 
 function correctOn() {
@@ -25,6 +67,8 @@ function correctOn() {
 
     let overlay = document.getElementById("overlay");
     overlay.style.display = "none";
+    let overlayTwo = document.getElementById("overlayTwo");
+    overlayTwo.style.display = "none";
 
 }
 function incorrectOff() {
@@ -35,56 +79,19 @@ function incorrectOff() {
 
     japan.addEventListener("click", incorrectOn);
     indonesia.addEventListener("click", incorrectOn);
+
+    let turkey = document.getElementById("turkey");
+    let brazil = document.getElementById("brazil");
+
+    turkey.addEventListener("click", incorrectOn);
+    brazil.addEventListener("click", incorrectOn);
 }
 
 function incorrectOn() {
     document.getElementById("incorrectPage").classList.remove("hide");
     document.getElementById("overlay").style.display = "none";
+    document.getElementById("overlayTwo").style.display = "none";
 }
 
 
-function overlayTwoOff(){
-let overlayTwo = getElementById("overlayTwo");
-overlayTwo.style.display = "none";
-let next = getElementsByClassName("next");
-next.addEventListener("click", overlayTwoOn)
-}
-
-function overlayTwoOn(){
-    document.getElementById("overlayTwo").classList.remove("hide");
-    document.getElementById("incorrectPage").style.display = "none";
-    document.getElementById("correctPage").style.display = "none";
-}
-
-
-function correctTwoOff() {
-    let correctPage = document.getElementById("correctPage");
-    correctPage.style.display = "none";
-    
-    let australia = document.getElementById("australia");
-    australia.addEventListener("click", correctTwoOn);
-}
-
-function correctTwoOn() {
-    document.getElementById("correctPage").classList.remove("hide");
-    correctPage.style.display = "visible";
-
-    let overlayTwo = document.getElementById("overlayTwo");
-    overlayTwo.style.display = "none";
-
-}
-function incorrectTwoOff() {
-
-    document.getElementById("incorrectPage").style.display = "none";
-    let turkey = document.getElementById("turkey");
-    let brazil = document.getElementById("brazil");
-
-    turkey.addEventListener("click", incorrectTwoOn);
-    brazil.addEventListener("click", incorrectTwoOn);
-}
-
-function incorrectTwoOn() {
-    document.getElementById("incorrectPage").classList.remove("hide");
-    
-}
 
